19b716b42894e4cecd54255cf5555f49256b3c22
Ajjugunti Banny Vardhan Reddy <bannyvardhan135@gmail.com>
2026-02-14 11:31:00 +0530

    Updated with syle.css

--- script.js
+++ script.js
@@ -1,7 +1,76 @@
-async function fetchUser(user_name) {
-    const response = await fetch(`https://api.github.com/users/${user_name}`);
-    if(!response.ok){
-        throw new Error("Error fetching User");
+const search = document.getElementById("search");
+search.addEventListener("click", getDetails);
+
+async function getDetails() {
+    document.querySelector(".output").style.visibility = "hidden";
+    document.getElementById("repos").innerHTML = "";
+    let username = document.getElementById("username");
+    console.log(username);
+    if (!username.value.trim()) {
+        return;
+    }
+
+    //Add a loading msg 
+    const loading_msg = document.getElementById("loading");
+    loading_msg.innerHTML = "Loading.....";
+
+
+    // try {
+    //     const [user_details, user_repos] = await Promise.all([fetchUserDetails(username.value.trim()), fetchUserRepos(username.value.trim())]);
+
+    //     //Remove loading message and username from input field
+    //     loading_msg.innerHTML = "";
+    //     username.value = "";  
+
+    //     const user_name = document.getElementById("user-name");
+    //     const user_avatar = document.getElementById("user-avatar")
+
+    //     user_name.innerHTML = user_details.name;
+    //     user_avatar.src = `${user_details.avatar_url}`;
+    //     console.log(user_repos);
+    //     // console.log(user_details.avatar_url);
+
+    // } catch(message) {
+    //     loading_msg.innerHTML = message;
+    // }
+
+    try{
+        const [user_details, user_repos] = await Promise.all([fetchUserDetails(username.value.trim()), fetchUserRepos(username.value.trim())]);
+
+        document.querySelector(".output").style.visibility = "visible";
+        loading_msg.innerHTML = "";
+        // username.value = "";
+        console.log(user_details);
+        const name = document.getElementById("name");
+        name.innerHTML = `
+            <p><strong>Name: </strong>${user_details.name}</p>
+            <p><strong>Bio: </strong>${user_details.bio}</p>
+            <p><strong>GitHub URL: </strong>${user_details.html_url}</p>
+            <p><strong>Location: </strong>${user_details.location}</p>
+        `;
+        
+        const avatar = document.getElementById("avatar");
+        // avatar.src = `${user_details.avatar_url}`;
+        avatar.style.backgroundImage = `url("${user_details.avatar_url}")`;
+        // console.log(user_repos);
+        const repos = document.getElementById("repos");
+        user_repos.forEach(repo => {
+            // console.log(repo);
+
+            const div = document.createElement("div");
+            div.classList.add("repo")
+            div.innerHTML  = `${repo.name}`
+            repos.appendChild(div);
+        });
+    }catch(err){
+        loading_msg.innerHTML = err.message;
+    }
+}
+
+async function fetchUserDetails(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if (!response.ok) {
+        throw new Error("Error fetching user details");
     }
 
     const data = await response.json();
@@ -9,49 +78,14 @@ async function fetchUser(user_name) {
     return data;
 }
 
-async function fetchRepo(user_name) {
-    const response = await fetch(`https://api.github.com/users/${user_name}/repos`);
+async function fetchUserRepos(username) {
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
     if(!response.ok){
-        throw new Error("Error fetching repos");
+        throw new Error("Error fetching user repos");
     }
 
-    const repo_data = await response.json();
-
-    return repo_data;
+    const data = await response.json();
+    console.log(data);
+    return data;
 }
-const img = document.createElement('img');
-async function getDetails(){
-    let user_name = document.getElementById("name").value.trim();
-    if(!user_name){
-        return;
-    }
-    img.src = "";
-    let loading_msg = document.getElementById("loading-msg");
-    let input_msg = document.getElementById("output-msg");
-    loading_msg.innerHTML = "Loading.....";
-    try{
-        const [data, repos] = await Promise.all([fetchUser(user_name),fetchRepo(user_name)]);
-        console.log(repos);
-        loading_msg.innerHTML = "";
-        input_msg.innerHTML = data.name;
-        console.log(data.name);
-        img.src = data.avatar_url;
-        document.getElementById("output").appendChild(img);
-        if(repos.length === 0){
-            document.getElementById('repo').innerHTML = "Zero Repos";
-        }
-        else{
-            document.getElementById('repo').innerHTML = "<h1>Repositories</h1>";
-            repos.forEach(repo => {
-                const repo_data = document.createElement('p');
-                repo_data.innerHTML = repo.name;
-                document.getElementById('repo').appendChild(repo_data)
-            });
-        }
-        user_name = "";
-    }catch (error){
-        console.log(error);
-    }
-}
-
-document.getElementById("submit").addEventListener("click",getDetails);
